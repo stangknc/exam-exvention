6540c1d59947e2f37f50b876210130a64766c3c1
62160076@go.buu.ac.th <tang_kanachart@hotmail.com>
2023-05-13 15:01:07 +0700

    president 80%
    **responsive**

--- exam-exvention/src/app/president/president.component.ts
+++ exam-exvention/src/app/president/president.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { PresidentService } from '../president/service/president.service';
 @Component({
   selector: 'app-president',
   templateUrl: './president.component.html',
   styleUrls: ['./president.component.css']
 })
 export class PresidentComponent {
+  presidents: any;
+  constructor(private PresidentService: PresidentService) { }
 
+  ngOnInit(): void {
+    this.getPresident();
+  }
+  getPresident() {
+    this.PresidentService.getPresident().subscribe(data =>{
+      this.presidents = data;
+      console.log(this.presidents);
+    })
+  }
 }
--- exam-exvention/src/app/president/service/president.service.ts
+++ exam-exvention/src/app/president/service/president.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class PresidentService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPresident(): Observable<Array<any>>{
+    return this.http.get<Array<any>>('assets/presidentData.json');
+  }
 }
